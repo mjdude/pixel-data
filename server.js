8c7eba6275a8f2d91a607b03d46150097de6b6fc
mjdude <mo@syncspot.net>
2016-05-06 20:30:25 +0100

    added get api for pixel data

--- server.js
+++ server.js
@@ -5,3 +5,13 @@ var app = express();
 app.get('/', function(req, res){
     console.log('Pixel-Data Server Root');
 } );
+
+// Log data fron client
+
+app.get('/img', function(req, res){
+
+});
+
+app.listen(PORT , function(){
+  console.log('starting server on ' + PORT);
+})
