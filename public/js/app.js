cec985e063a945fbec23243d2b70e079a798f328
mjdude <mo@syncspot.net>
2016-05-07 12:41:14 +0100

    pixel data mvp now working

--- public/js/app.js
+++ public/js/app.js
@@ -2,4 +2,9 @@ var $form = jQuery('#email-form');
 $form.on('submit', function(event){
   event.preventDefault();
   var $email = $form.find('input[name=email]');
+//  var $email = 'mo@syncspot.net';
+  console.log($email.val());
+  var imgHTML = '<img src="http://localhost:3000/pixel?orderID=12345&orderVal=99.95&email=' + $email.val() + '">';
+  console.log(imgHTML);
+  $("#pixel-data").html(imgHTML);
 });
